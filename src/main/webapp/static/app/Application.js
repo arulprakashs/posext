6d29afa3f36d401287b3b5a7428ea9a8b1365657
swaminathana <swaminathana@ncs.com.sg>
2014-12-02 13:49:05 +0800

    Removed Application.js

--- src/main/webapp/static/app/Application.js
+++ src/main/webapp/static/app/Application.js
@@ -1,4 +1,4 @@
-Ext.define('pos.Application', {
+/*Ext.define('pos.Application', {
     name: 'pos',
 
     extend: 'Ext.app.Application',
@@ -17,3 +17,4 @@ Ext.define('pos.Application', {
     	'Users'
     ]
 });
+*/
